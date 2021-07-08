81beca397535d50830162af7a107c8ab4f64c9d1
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-07-08 17:59:59 +0200

    cmake: mark WinMain as an undefined symbol, when building with mingw

--- cmake/win/WinFunctions.cmake
+++ cmake/win/WinFunctions.cmake
@@ -4,4 +4,17 @@ set(GLFW3_STATIC_INIT ON)
 set(LIBRW_STATIC_RUNTIME_DEFAULT ON)
 
 function(librw_platform_target TARGET)
+    cmake_parse_arguments(LPT "PROVIDES_WINMAIN" "" "" ${ARGN})
+    get_target_property(TARGET_TYPE "${TARGET}" TYPE)
+
+    if(MINGW)
+        if(LPT_PROVIDES_WINMAIN AND TARGET_TYPE MATCHES "^(INTERFACE|SHARED|STATIC)_LIBRARY$")
+            # Start with the WinMain symbol marked as undefined.
+            # This will prevent the linker ignoring a WinMain symbol.
+            if(CMAKE_SIZEOF_VOID_P EQUAL 4)
+                target_link_options(${TARGET} INTERFACE -Wl,--undefined,_WinMain@16)
+            endif()
+            target_link_options(${TARGET} INTERFACE -Wl,--undefined,WinMain)
+        endif()
+    endif()
 endfunction()
